94732fd925d6c870c97563c1047ab57525572187
Pramudit <spramudit@gmail.com>
2017-10-04 19:39:15 +0530

    loginfirse

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Ingenium',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{
   useMongoClient : true
 })
+
 
 
 module.exports = {
